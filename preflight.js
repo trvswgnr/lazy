6772cecf484c13cf1ef74d97e9425110b7d62778
Travis Wagner <travis.wagner@umusic.com>
2024-06-01 20:18:26 -0400

    update

--- preflight.js
+++ preflight.js
@@ -1,16 +1,18 @@
 // @ts-check
 import { readFileSync, writeFileSync } from "fs";
 
+const CONFIG_PATH = "./jsr.json";
+
 /**
- * @type {PackageJson}
+ * @type {Config}
  */
-const packageJson = JSON.parse(readFileSync("./package.json", "utf8"));
+const config = JSON.parse(readFileSync(CONFIG_PATH, "utf8"));
 
 const SEMVER_REGEX =
     /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 
 // increment the version
-packageJson.version = packageJson.version.replace(
+config.version = config.version.replace(
     SEMVER_REGEX,
     (
         /** @type {string} */ _,
@@ -22,10 +24,18 @@ packageJson.version = packageJson.version.replace(
     },
 );
 
-writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
+writeFileSync(CONFIG_PATH, JSON.stringify(config, null, 2));
 
 /**
- * @typedef {{
- *     version: string;
- * }} PackageJson
+ * @typedef {Object} Config
+ * @property {string} version The version of this JSR package.
+ * @property {Object.<string, string> | string} exports A JSON representation of exports in a JSR configuration file.
+ * @property {string} name The name of this JSR package. Must be scoped.
+ * @property {Publish} [publish] Optional publishing details.
+ */
+
+/**
+ * @typedef {Object} Publish
+ * @property {string[]} [exclude] List of files, directories, or globs that will be excluded from the published package.
+ * @property {string[]} [include] List of files, directories, or globs that will be included in the published package.
  */
