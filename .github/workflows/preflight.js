634e5e46d89bda1a4eec13453ed77c368d6f045e
Travis Wagner <travis.wagner@umusic.com>
2024-06-01 20:06:55 -0400

    add publish action
    also added preflight.js to bump version

---
+++ .github/workflows/preflight.js
@@ -0,0 +1,37 @@
+// @ts-check
+const { readFileSync, writeFileSync } = require("fs");
+const { join } = require("path");
+
+/**
+ * @type {PackageJson}
+ */
+const packageJson = JSON.parse(
+    readFileSync(join(__dirname, "../package.json"), "utf8"),
+);
+
+const SEMVER_REGEX =
+    /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+
+// increment the version
+packageJson.version = packageJson.version.replace(
+    SEMVER_REGEX,
+    (
+        /** @type {string} */ _,
+        /** @type {string} */ major,
+        /** @type {string} */ minor,
+        /** @type {string} */ patch,
+    ) => {
+        return `${major}.${minor}.${parseInt(patch) + 1}`;
+    },
+);
+
+writeFileSync(
+    join(__dirname, "../package.json"),
+    JSON.stringify(packageJson, null, 2),
+);
+
+/**
+ * @typedef {{
+ *     version: string;
+ * }} PackageJson
+ */
